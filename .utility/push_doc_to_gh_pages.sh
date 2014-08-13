28b7ce579051aaf10b87823d8b7012060c829555
tiago <tiago.nobre@mendeley.com>
2014-08-13 12:38:02 +0100

    remove `

--- .utility/push_doc_to_gh_pages.sh
+++ .utility/push_doc_to_gh_pages.sh
@@ -9,8 +9,6 @@ if [ "$TRAVIS_REPO_SLUG" == "macwadu/jira-rest" ] && [ "$TRAVIS_PULL_REQUEST" ==
   cp -R doc/ $HOME/doc-latest
 
   cd $HOME
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/macwadu/jira-rest gh-pages > /dev/null
 
   cd gh-pages
@@ -18,7 +16,7 @@ if [ "$TRAVIS_REPO_SLUG" == "macwadu/jira-rest" ] && [ "$TRAVIS_PULL_REQUEST" ==
   cp -Rf $HOME/doc-latest ./doc
   git add -f .
   git commit -m "Lastest doc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_SLUG}.git gh-pages'
+  git push -fq https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 
   echo -e "Published doc to gh-pages.\n"
   
